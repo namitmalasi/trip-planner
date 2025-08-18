b2a9395f703a85d41af14c03f3809522ed64f9ef
Namit Malasi <namitmalasi@gmail.com>
2025-08-18 20:25:54 +0530

    setting up project structure

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+import { Button } from '@/components/ui/button'
+function App() {
+
+  return (
+    <>
+      <h2>hello world</h2>
+      <Button variant="outline">Click me</Button>
+    </>
+  )
+}
+
+export default App
